8a180986d4eedab61e23c9be79f93c7f2a688a86
JongIn Yoon <bellpp93@gmail.com>
2022-05-09 17:15:32 +0900

    0509_각 화폐 단위로 출력하는 파이썬 프로그램 예제

--- 0509/ChangeMoney_Ex.py
+++ 0509/ChangeMoney_Ex.py
@@ -5,7 +5,7 @@ money = int(input("금액을 입력해 주세요 >>> "))
 
 list_money = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
 list_str_money = ["오만원권","만원권","오천원권","천원권","오백원","백원","오십원","십원","일원"]
-# divmod는 몫과 나머지 값을 한번에 구하는 내장함수(built in 함수)이다. portion은 몫, remainder는 나머지 값.
+
 for i in range(0,9):
     portion, remainder = divmod(money, list_money[i])
     money = remainder
@@ -15,6 +15,7 @@ for i in range(0,9):
     elif portion > 0 and i >= 4:
         print(list_str_money[i] + ' ' + str(portion) + '개')
 
+# divmod는 몫과 나머지 값을 한번에 구하는 내장함수(built in 함수)이다. portion은 몫, remainder는 나머지 값.
 
 '''
 money, c50000, c10000, c5000, c1000, c500, c100, c50, c10 = 0,0,0,0,0,0,0,0,0
