a784fd62ccd0abeb68d5796e2f92f40ffb122f76
JongIn Yoon <bellpp93@gmail.com>
2022-05-09 14:11:14 +0900

    0504_Python_first_Ex

--- 0504/Ex02.py
+++ 0504/Ex02.py
@@ -1,3 +1,4 @@
 for i in range(1, 11):
     #print(i) #print()는 기본적으로 줄바꿈을 한다. '\n'
     print(i, end=' ') #줄바꿈을 안하겠다.
+# 1 2 3 4 5 6 7 8 9 10
--- 0506/Class_Ex01.py
+++ 0506/Class_Ex01.py
@@ -2,8 +2,8 @@ class MyClass:
     var = '안녕하세요'  # 클래스 멤버
 
     def sayHello(self):  # 클래스 메소드
-        print(self.var)
+        print(self.var)  # 안녕하세요
 
 obj = MyClass()  # obj는 객체참조변수, MyClass 인스턴스 객체 생성
-print(obj.var)
+print(obj.var)  # 안녕하세요
 obj.sayHello()
