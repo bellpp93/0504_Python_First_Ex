abe98f2c8768473b7e77b81979aa1ddfbd65538b
JongIn Yoon <bellpp93@gmail.com>
2022-05-09 16:06:19 +0900

    0506_클래스 예제

--- 0506/Class_Ex01.py
+++ 0506/Class_Ex01.py
@@ -5,5 +5,5 @@ class MyClass:
         print(self.var)
 
 obj = MyClass()  # obj는 객체참조변수, MyClass 인스턴스 객체 생성
-print(obj.var)  # 안녕하세요
-obj.sayHello()  # 안녕하세요
+print(obj.var)  # [출력] 안녕하세요
+obj.sayHello()  # [출력] 안녕하세요
