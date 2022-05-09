befe27bcf66d458178075eadee6dad9879fff8d6
JongIn Yoon <bellpp93@gmail.com>
2022-05-09 16:15:02 +0900

    0509_리스트, 튜플 자료구조 예제

--- 0509/List_Ex01.py
+++ 0509/List_Ex01.py
@@ -5,12 +5,12 @@ list1 = [10,20,30,40,50]
 list2 = ['a','b','c']
 list3 = [10,'a','abc',[10,20,30,40,50],['a','b','c']]
 list1[0] = 60  # 리스트의 요소는 변경 가능
-print(list1)  # [60, 20, 30, 40, 50]
+print(list1)  # [출력] [60, 20, 30, 40, 50]
 
 def myfunc():
     print('Python Good!!')
 list4 = [10,20,myfunc]
-list4[2]()  # Python Good!!
+list4[2]()  # [출력] Python Good!!
 
 
 """
@@ -20,10 +20,10 @@ tuple1 = (10,20,30,40,50)
 tuple2 = ('a','b','c')
 tuple3 = (10,'a','abc',[10,20,30,40,50],['a','b','c'])
 # tuple1[0] = 60  # 튜플의 요소는 변경 불가능
-print(tuple1)  # (10, 20, 30, 40, 50)
+print(tuple1)  # [출력] (10, 20, 30, 40, 50)
 
 def myfunc():
     print('Python Good!!')
 
 tuple4 = (10,20,myfunc)
-tuple4[2]()  # Python Good!!
+tuple4[2]()  # [출력] Python Good!!
