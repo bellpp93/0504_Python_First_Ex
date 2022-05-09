dfb8a56e554c6a811de2a91cc13da25ee1b6356e
JongIn Yoon <bellpp93@gmail.com>
2022-05-09 16:10:49 +0900

    0506_파이썬 문제 예제

--- 0506/PythonTest_Ex.py
+++ 0506/PythonTest_Ex.py
@@ -1,3 +1,6 @@
+""" 문자열 자료 슬라이싱 => [시작인덱스:끝인덱스:스텝]
+시작인덱스 '이상'부터 끝인덱스 '미만'까지 가져온다. """
+
 strdata = 'Time is money!!'
 listdata = [1, 2, [1,2,3]]
 
@@ -6,9 +9,6 @@ print(strdata[-2])  # [정답] !
 print(listdata[0])  # [정답] 1
 print(listdata[-1])  # [정답] [1,2,3]
 print(listdata[2][-1])  # [정답] 3
-
-""" 문자열 자료 슬라이싱 => [시작인덱스:끝인덱스:스텝]
-시작인덱스 '이상'부터 끝인덱스 '미만'까지 가져온다. """
 
 print(strdata[1:5])  # [정답] ime(공백)
 print(strdata[:7])  # [정답] Time is
